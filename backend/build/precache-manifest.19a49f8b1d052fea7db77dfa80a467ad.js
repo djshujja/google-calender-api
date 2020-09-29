7a9955093f94b2c2e7e0aceec66f0afac0246020
djshujja <djshujja@gmail.com>
2020-09-29 05:00:47 +0500

    added node backend

---
+++ backend/build/precache-manifest.19a49f8b1d052fea7db77dfa80a467ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d5a04910f1bbf7d8d60f5340e00ec6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d5f891083b4eb538e02",
+    "url": "/static/css/main.6d61ae4e.chunk.css"
+  },
+  {
+    "revision": "99d78da8d7ed73352862",
+    "url": "/static/js/2.a82234a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a82234a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5f891083b4eb538e02",
+    "url": "/static/js/main.41a7ad6d.chunk.js"
+  },
+  {
+    "revision": "b9268a4b7df90c8d2488",
+    "url": "/static/js/runtime-main.5535a399.js"
+  }
+]);
